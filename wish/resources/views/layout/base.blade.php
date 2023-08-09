08b018eb294c71670bc4000df175c07f7bdbfbe8
RafiDevari <rafidevari2004@gmail.com>
2023-08-09 21:34:06 +0700

    test

---
+++ wish/resources/views/layout/base.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body style="background-image: url('https://th.bing.com/th/id/OIP.NybRdXatKIsUS844cVtEaAAAAA?pid=ImgDet&rs=1'); background-repeat: no-repeat; background-size: 120%;">
+    
+    @yield('navbar')
+
+    @yield('konten')
+
+    @yield('footer')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ wish/resources/views/main.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.base')
+
+@section('konten')
+    
+@endsection
+
+@section('navbar')
+    @extends('layout.navbar')
+@endsection
