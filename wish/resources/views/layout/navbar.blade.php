36406e42071ab967c627982e33ae7ff4fd6e7896
RafiDevari <rafidevari2004@gmail.com>
2023-08-11 16:51:25 +0700

    useless

--- wish/resources/views/layout/navbar.blade.php
+++ wish/resources/views/layout/navbar.blade.php
@@ -1,29 +1,42 @@
-<div class="navbar">
-  <div class="navbar-card">
-    
-        {{-- suiitable height/width = 50/100 px --}}
-    <a id="1" onclick="change(this.id)" href="#">
-      <div class="imgcont">
-        <img class="navimg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/560px-PNG_transparency_demonstration_1.png" alt="">
-      </div>
-    </a>
-    <a id="2" onclick="change(this.id)" href="#">
-      <div class="imgcont">
-        <img class="navimg" src="https://4.bp.blogspot.com/-sL27hk1Aq30/VxsWEnra7PI/AAAAAAAAE_s/Bp0iEq8srb4m8xQpgbYH1Zh_ylfIicBfACLcB/s320/3dAletter.png" alt="">
-      </div>
-    </a>
-    <a id="3" onclick="change(this.id)" href="#">
-      <div class="imgcont">
-        <img class="navimg" src="https://3.bp.blogspot.com/--qjkLEY-CLo/VxsWIsHb2BI/AAAAAAAAE_w/86yz5MkdSQkUk_pTxDryFwQ3GGrKfAHzwCLcB/s320/3dBletter.png " alt="">
-      </div>
-    </a>
-    <a id="4" onclick="change(this.id)" href="#">
-      <div class="imgcont">
-        <img class="navimg" src="https://4.bp.blogspot.com/-elNbDyMXaRc/VxsWEYpS4uI/AAAAAAAAE_o/mNtCp07G-voaDBSyR5wkdf5hNoXtGwq8wCLcB/s320/3dCletter.png" alt="">
-      </div>
-    </a>
-    
-  </div>
-</div>
 
-<h5 id="tes">Hello</h5>
+<table style="width: 100%" border="0"><tr>
+
+    <td>
+        <img src="http://www.rw-designer.com/icon-view/22723.png" alt="">
+        <h5>Wish</h5>
+    </td>
+
+    <td>
+      <div class="navbar">
+        <div class="navbar-card">
+              {{-- suiitable height/width = 50/100 px --}}
+          <a id="1" onclick="change(this.id)" href="#">
+            <div class="imgcont">
+              <img class="navimg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/560px-PNG_transparency_demonstration_1.png" alt="">
+            </div>
+          </a>
+          <a id="2" onclick="change(this.id)" href="#">
+            <div class="imgcont">
+              <img class="navimg" src="https://4.bp.blogspot.com/-sL27hk1Aq30/VxsWEnra7PI/AAAAAAAAE_s/Bp0iEq8srb4m8xQpgbYH1Zh_ylfIicBfACLcB/s320/3dAletter.png" alt="">
+            </div>
+          </a>
+          <a id="3" onclick="change(this.id)" href="#">
+            <div class="imgcont">
+              <img class="navimg" src="https://3.bp.blogspot.com/--qjkLEY-CLo/VxsWIsHb2BI/AAAAAAAAE_w/86yz5MkdSQkUk_pTxDryFwQ3GGrKfAHzwCLcB/s320/3dBletter.png " alt="">
+            </div>
+          </a>
+          <a id="4" onclick="change(this.id)" href="#">
+            <div class="imgcont">
+              <img class="navimg" src="https://4.bp.blogspot.com/-elNbDyMXaRc/VxsWEYpS4uI/AAAAAAAAE_o/mNtCp07G-voaDBSyR5wkdf5hNoXtGwq8wCLcB/s320/3dCletter.png" alt="">
+            </div>
+          </a>
+          
+        </div>
+      </div>
+    </td>
+
+    <td>
+      Ini Total primo
+    </td>
+
+</tr></table>
